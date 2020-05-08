1e692a87803923a58e4862fae75a26e175f527f4
nbaramkiazar <naseembaramkiazar@gmail.com>
2020-05-07 23:59:28 -0700

    Lesson 5A

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        transform.position = new Vector3(Random.Range(-5, 5), -5);
+        targetRB.AddForce(Vector3.up * Random.Range(14, 19), ForceMode.Impulse);
+        targetRB.AddTorque(Random.Range(-5, 5), Random.Range(-5, 5),
+            Random.Range(-5, 5), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
