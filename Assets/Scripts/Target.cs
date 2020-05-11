e323614fc0738d6c1e024188b95cd5df4a86a827
nbaramkiazar <naseembaramkiazar@gmail.com>
2020-05-11 11:52:34 -0700

    Lesson 5B

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRB;
+    private EventManager eventManager;
+    [SerializeField] private ParticleSystem expParticle;
     // Start is called before the first frame update
     void Start()
     {
+        eventManager = GameObject.Find("EventManager").GetComponent<EventManager>();
         targetRB = GetComponent<Rigidbody>();
         transform.position = new Vector3(Random.Range(-5, 5), -5);
         targetRB.AddForce(Vector3.up * Random.Range(14, 19), ForceMode.Impulse);
@@ -22,7 +25,20 @@ public class Target : MonoBehaviour
     }
     private void OnMouseDown()
     {
+        Instantiate(expParticle, transform.position, expParticle.transform.rotation);
+
         Destroy(gameObject);
+
+        if(CompareTag("Bad Target"))
+        {
+            //gameManager.UpdateScore(-7);
+                eventManager.targetDestroyed.Invoke(-7);
+        }
+        else if(CompareTag("Good Target"))
+        {
+            //gameManager.UpdateScore(7);
+            eventManager.targetDestroyed.Invoke(7);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
